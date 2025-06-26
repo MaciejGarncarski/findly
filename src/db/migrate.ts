d28c92163a6df01319dcea11e3449198d58fe66a
Maciej Garncarski <maciejgarncarski@protonmail.com>
2025-06-26 10:59:10 +0200

    feat: improve db migrations

--- src/db/migrate.ts
+++ src/db/migrate.ts
@@ -1,16 +1,23 @@
 import dotenv from "dotenv";
 import path from "path";
 import { migrate } from "drizzle-orm/postgres-js/migrator";
-import { db } from "./client";
+import { db } from "./client.ts";
 
 dotenv.config();
 
 async function main() {
-  await migrate(db, {
-    migrationsFolder: path.join(process.cwd(), "./drizzle"),
-  });
-  console.log(`Migrations complete`);
-  process.exit(0);
+
+  try {
+    await migrate(db, {
+      migrationsFolder: path.join(process.cwd(), "./drizzle"),
+    });
+    console.log("Migration successful");
+  } catch (error) {
+    console.error(error);
+    console.log("Migration failed");
+    process.exit(1);
+  }
+
 }
 
 main();
